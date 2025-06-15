1e41fce5acf967b819592320982aacd1b48a0d92
Jenna Deamer <jennadeamer@gmail.com>
2025-06-15 14:24:36 -0400

    Refactor slideshow html struc, refactor styles, add slideshow moving on its own or with click

---
+++ script.js
@@ -0,0 +1,35 @@
+const slides = document.querySelectorAll(".slide img");
+
+let slideIndex = 0;
+let interval = null;
+
+const initializeSliderInterval = () => {
+  interval = setInterval(nextSlide, 2000);
+};
+
+const displaySlide = (index) => {
+  // Ensure all slides are hidden
+  slides.forEach((slide) => {
+    slide.classList.remove("display-slide");
+  });
+  // Display next slide
+  slides[slideIndex].classList.add("display-slide");
+};
+
+const nextSlide = () => {
+  slideIndex++;
+  displaySlide(slideIndex);
+};
+
+const prevSlide = () => {};
+
+// Attach event listeners
+const nextSlideBtn = document
+  .querySelector(".next")
+  .addEventListener("click", nextSlide);
+const prevSlideBtn = document
+  .querySelector(".prev")
+  .addEventListener("click", prevSlide);
+
+
+initializeSliderInterval();
